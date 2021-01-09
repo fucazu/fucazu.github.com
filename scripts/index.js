7f6ef58cbc7213f3ab6345e93528712b4df21070
Rafael Sanches <rafaelsanchesdev@gmail.com>
2021-01-09 19:14:19 -0300

    Adding logo animation

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('logo'),
+  path: 'data.json',
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  name: "Logo",
+})
+let direction = 1;
+animation.onComplete = function () {
+  direction = direction * -1;
+  animation.setDirection(direction);
+  if (direction === 1) {
+    setTimeout(() => animation.play(), 3000);
+  } else {
+    animation.play();
+  }
+};
+
+
+window.addEventListener('resize', function () {
+  animation.resize();
+});
